11869b6815f554c5da4dee5ada3c5354db0c7329
nalindadsn <nalindadsn@gmail.com>
2022-10-06 18:06:38 +0530

    b

--- components/Layout.js
+++ components/Layout.js
@@ -237,22 +237,6 @@ export default function Layout({ title, children }) {
                     </a>
                   </Link>
                 )}
-                <button className="focus:outline-none bg-gray-100 text-gray-400 hover:text-gray-800 w-7 h-7 rounded-full flex items-center justify-center">
-                  <svg
-                    className="w-4 h-4"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    ></path>
-                  </svg>
-                </button>
               </div>
             </main>
           </nav>
