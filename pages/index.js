e30e6acaf857c18282f2f5bde7da6b0ac5a05a1c
nalindadsn <nalindadsn@gmail.com>
2022-11-05 23:42:36 +0530

    add

--- pages/index.js
+++ pages/index.js
@@ -137,6 +137,41 @@ export default function Home({ topRatedProducts, featuredProducts }) {
           </a>
         </Link>
       </div>
+      <section className="text-gray-700 body-font relative">
+        <div className="absolute inset-0 bg-gray-300">
+          <iframe
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            marginHeight="0"
+            marginWidth="0"
+            title="map"
+            scrolling="no"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4047271.299976242!2d78.46169489521603!3d7.851731513542368!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2593cf65a1e9d%3A0xe13da4b400e2d38c!2sSri%20Lanka!5e0!3m2!1sen!2slk!4v1667671838120!5m2!1sen!2slk"
+            style={{ filter: ' grayscale(1) contrast(1.2) opacity(0.4)' }}
+          ></iframe>
+        </div>
+        <div className="container px-5 py-24 mx-auto flex">
+          <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10">
+            <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+              Feedback
+            </h2>
+            <p className="leading-relaxed mb-5 text-gray-600"></p>
+            <input
+              className="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
+              placeholder="Email"
+              type="email"
+            />
+            <textarea
+              className="bg-white rounded border border-gray-400 focus:outline-none h-32 focus:border-indigo-500 text-base px-4 py-2 mb-4 resize-none"
+              placeholder="Message"
+            ></textarea>
+            <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              Button
+            </button>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 }
