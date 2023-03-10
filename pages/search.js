a591f4618daffa9e29e0235d2b92267250b598ac
Nalindadsn <nalindadsn@gmail.com>
2023-03-10 22:20:34 +0530

    add new fixed

--- pages/search.js
+++ pages/search.js
@@ -118,12 +118,12 @@ export default function Search(props) {
         >
           <div className="bg-gray-900 px-4 py-2 font-bold text-white">
             
-            <div class="mb-4 flex items-center">
+            <div className="mb-4 flex items-center">
             FILTER
              
-              <div class="flex gap-2 ml-auto">
+              <div className="flex gap-2 ml-auto">
                 <span className="lg:hidden">
-                                  <svg onClick={toggleChecked} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-5 w-5 font-semibold hover:text-red-500 text-red-600 text-xs"><path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                  <svg onClick={toggleChecked} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" className="h-5 w-5 font-semibold hover:text-red-500 text-red-600 text-xs"><path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
 
                 </span>
               </div>
