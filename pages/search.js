ad3fd7c28d4d8d9be8a07fabf1ff7a05e30fd996
Nalindadsn <nalindadsn@gmail.com>
2023-03-10 22:24:06 +0530

    add new fixed2

--- pages/search.js
+++ pages/search.js
@@ -123,7 +123,7 @@ export default function Search(props) {
              
               <div className="flex gap-2 ml-auto">
                 <span className="lg:hidden">
-                                  <svg onClick={toggleChecked} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" className="h-5 w-5 font-semibold hover:text-red-500 text-red-600 text-xs"><path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                  <svg onClick={toggleChecked} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" ariaHidden="true" className="h-5 w-5 font-semibold hover:text-red-500 text-red-600 text-xs"><path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
 
                 </span>
               </div>
