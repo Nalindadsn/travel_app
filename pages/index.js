1a216d391f084fb5cd2fdced45e16d873f5f450f
Nalindadsn <90615024+Nalindadsn@users.noreply.github.com>
2023-06-07 07:58:19 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home({ topRatedPosts, featuredPosts }) {
           height: '36rem',
           backgroundColor: '#333',
           backgroundImage:
-            "url('https://res.cloudinary.com/masterdevs/image/upload/v1686037553/4yBAzK_wft8th.jpg')",
+            "url('https://res.cloudinary.com/masterdevs/image/upload/v1686124301/3901505_jn9bo1.jpg')",
           backgroundSize: 'cover',
           backgroundPosition: 'center',
         }}
