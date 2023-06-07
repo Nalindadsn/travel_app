80edd9ffb861f87c8a0fa26cf1b133f1d03274b0
Nalindadsn <90615024+Nalindadsn@users.noreply.github.com>
2023-06-07 10:48:44 +0000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import Layout from '../components/Layout';
 
 function about() {
-  return <Layout>  <div className="flex items-right   float-right flex-grow pl-12">
+  return <Layout>  <div className="flex items-right   float-right flex-grow pl-12" style={{ marginLeft: '5%', marginRight: '5%' }}>
                       <div className="w-full text-right">
                         <Link href="/">
                           <a className=" inline-block hover:border-gray hover:text-gray border-b border-lisgt-900 px-2 transition">
@@ -38,7 +38,7 @@ function about() {
                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 </div>
   <div>
-                       <img style={{width:"100%"}} src="https://res.cloudinary.com/masterdevs/image/upload/v1686134248/feature1_afvlur.png"/></div>
+                       <img style={{width:"50%",float:"left"}} src="https://res.cloudinary.com/masterdevs/image/upload/v1686134248/feature1_afvlur.png"/></div>
 </div>
   
   </Layout>;
