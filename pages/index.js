8e375b5af75d642931ba78f33e6ddc96f9164c80
Nalindadsn <90615024+Nalindadsn@users.noreply.github.com>
2023-06-07 11:32:46 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -144,7 +144,6 @@ export default function Home({ topRatedPosts, featuredPosts }) {
                                 <h4 className="text-3xl font-semibold tracking-tight text-gray-900 mb-4">WE ARE</h4>
                                 <p className="text-gray-700 mb-4">
                                   What is Lorem Ipsum
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                                   </p>
                                 
                             </div>
