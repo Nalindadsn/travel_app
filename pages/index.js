4ff4f5ca8e7d21204f1e9f88fb0c1e6c439db694
Nalindadsn <90615024+Nalindadsn@users.noreply.github.com>
2023-06-07 07:30:06 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -149,7 +149,7 @@ Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
  It has survived not only five centuries, but also the leap into electronic typesetting, 
  remaining essentially unchanged. It was popularised in the 1960s with the release of 
  Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
- software like Aldus PageMaker including versions of Lorem Ipsum.
+ software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                           </div>
                         </div>
                     </div>
