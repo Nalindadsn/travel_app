69747077cb0a8c74f100b64e86204a04c23c990c
Nalindadsn <90615024+Nalindadsn@users.noreply.github.com>
2023-06-07 14:41:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -67,22 +67,22 @@ export default function Home({ topRatedPosts, featuredPosts }) {
                     <div className="flex items-right   float-right flex-grow pl-12">
                       <div className="w-full text-right">
                         <Link href="/">
-                          <a className=" inline-block hover:border-gray hover:text-gray border-b border-lisgt-900 px-2 transition">
+                          <a className="text-white inline-block hover:border-gray hover:text-gray border-b border-lisgt-900 px-2 transition">
                             Home
                           </a>
                         </Link>
                         <Link href="/search">
-                          <a className=" inline-block hover:border-gray hover:text-gray border-b border-light-800 px-2 transition">
+                          <a className="text-white inline-block hover:border-gray hover:text-gray border-b border-light-800 px-2 transition">
                             Courses
                           </a>
                         </Link>
                         <Link href="/about">
-                          <a className=" inline-block hover:border-gray hover:text-gray border-b border-light-800 px-2 transition">
+                          <a className="text-white inline-block hover:border-gray hover:text-gray border-b border-light-800 px-2 transition">
                             About Us
                           </a>
                         </Link>
                         <Link href="/contact">
-                          <a className=" inline-block hover:border-gray hover:text-gray border-b border-light-800 px-2 transition">
+                          <a className="text-white inline-block hover:border-gray hover:text-gray border-b border-light-800 px-2 transition">
                             Contact Us
                           </a>
                         </Link>
