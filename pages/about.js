7e223f36a20f276b45b57b195452629deae93800
Nalindadsn <90615024+Nalindadsn@users.noreply.github.com>
2023-06-07 10:40:31 +0000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -37,7 +37,8 @@ function about() {
   <div>                              <h1 className="mb-4 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">About Us</h1>
                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 </div>
-  <div><img style={{width:10%}} src="https://res.cloudinary.com/masterdevs/image/upload/v1686134248/feature1_afvlur.png"/></div>
+  <div>
+                       <img style={{width:"100%"}} src="https://res.cloudinary.com/masterdevs/image/upload/v1686134248/feature1_afvlur.png"/></div>
 </div>
   
   </Layout>;
