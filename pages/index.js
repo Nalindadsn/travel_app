a42e1c308e71f8e14a5c4ee426261c61e25c64e2
Nalindadsn <90615024+Nalindadsn@users.noreply.github.com>
2023-06-07 07:17:31 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -142,7 +142,8 @@ export default function Home({ topRatedPosts, featuredPosts }) {
                                 <h3 className="font-semibold text-main-blue text-lg mb-4 font-regular">TRAVEL</h3>
                                 <h4 className="text-3xl font-semibold tracking-tight text-gray-900 mb-4">WE ARE</h4>
                                 <p className="text-gray-700 mb-4">Dream 
-Safari is a Dynamic Travel and Destination promoting Company. Mainly focusing on Sabaragamuwa and Uva Provinces in Sri Lanka. Our Safari Tours are well managed and structured for any type of traveller.</p>
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
                                 
                             </div>
                         </div>
