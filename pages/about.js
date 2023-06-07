ca77fd16246b36a2084c3d778b1db437e0c5bee5
Nalindadsn <90615024+Nalindadsn@users.noreply.github.com>
2023-06-07 11:03:59 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,30 @@ import React from 'react';
 import Layout from '../components/Layout';
 
 function about() {
-  return <Layout>about</Layout>;
+  return <Layout>  <div className="flex items-right   float-right flex-grow pl-12">
+                      <div className="w-full text-right">
+                        <Link href="/">
+                          <a className=" inline-block hover:border-gray hover:text-gray border-b border-lisgt-900 px-2 transition">
+                            Home
+                          </a>
+                        </Link>
+                        <Link href="/search">
+                          <a className=" inline-block hover:border-gray hover:text-gray border-b border-light-800 px-2 transition">
+                            Courses
+                          </a>
+                        </Link>
+                        <Link href="/about">
+                          <a className=" inline-block hover:border-gray hover:text-gray border-b border-light-800 px-2 transition">
+                            About Us
+                          </a>
+                        </Link>
+                        <Link href="/contact">
+                          <a className=" inline-block hover:border-gray hover:text-gray border-b border-light-800 px-2 transition">
+                            Contact Us
+                          </a>
+                        </Link>
+                      </div>
+                    </div></Layout>;
 }
 
 export default about;
